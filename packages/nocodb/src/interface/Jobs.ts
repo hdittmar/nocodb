f7f49400df1fedd94517829a918cf894495885a3
mertmit <mertmit99@gmail.com>
2026-04-25 07:04:29 +0000

    feat: sandbox cache als

--- packages/nocodb/src/interface/Jobs.ts
+++ packages/nocodb/src/interface/Jobs.ts
@@ -183,6 +183,10 @@ export interface DuplicateBaseJobData extends JobData {
     excludeScripts?: boolean;
     excludeDashboards?: boolean;
     excludeWorkflows?: boolean;
+    excludeDocuments?: boolean;
+    excludePersonalViews?: boolean;
+    excludePermissions?: boolean;
+    excludeRls?: boolean;
   };
 }
 
