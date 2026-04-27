0d32bd98e2960a8520412c269aa5fc742df08fc2
Raju Udava <86527202+dstala@users.noreply.github.com>
2026-04-27 07:44:07 +0000

    fix: review follow-ups for OpenForm button PR

--- packages/nc-gui/components/smartsheet/grid/canvas/composables/useCopyPaste.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/composables/useCopyPaste.ts
@@ -138,7 +138,7 @@ export function useCopyPaste({
   }
   actionManager: ActionManager
 }) {
-  const { $api } = useNuxtApp()
+  const { $api, $e } = useNuxtApp()
   const { isDataReadOnly } = useRoles()
   const { getMeta, metas } = useMetas()
   const { isMysql, isPg } = useBase()
@@ -1462,6 +1462,7 @@ export function useCopyPaste({
                 const url = await actionManager.resolveFormEditUrl(columnObj.id!, rowPk)
                 if (url) {
                   await copy(url)
+                  $e('c:button:open-form:copy-url', { via: 'keyboard' })
                   message.toast(t('msg.info.copiedToClipboard'))
                   return
                 }
--- packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
+++ packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
@@ -49,10 +49,10 @@ import * as nc_202604030000_installations_add_fk_user_id from './v0/nc_202604030
 import * as nc_202604040000_gcp_marketplace from './v0/nc_202604040000_gcp_marketplace';
 import * as nc_202604071200_default_org from './v0/nc_202604071200_default_org';
 import * as nc_202604071201_scim_config_default_role from './v0/nc_202604071201_scim_config_default_role';
-import * as nc_202604090000_button_open_form from './v0/nc_202604090000_button_open_form';
 import * as nc_202604100000_audit_org_id from './v0/nc_202604100000_audit_org_id';
 import * as nc_202604160000_docs_in_data from './v0/nc_202604160000_docs_in_data';
 import * as nc_202604200002_trash_cleanup_due_at from './v0/nc_202604200002_trash_cleanup_due_at';
+import * as nc_202604220000_button_open_form from './v0/nc_202604220000_button_open_form';
 
 // Create a custom migration source class
 export default class XcMigrationSourcev0 {
@@ -112,10 +112,10 @@ export default class XcMigrationSourcev0 {
       'nc_202604040000_gcp_marketplace',
       'nc_202604071200_default_org',
       'nc_202604071201_scim_config_default_role',
-      'nc_202604090000_button_open_form',
       'nc_202604100000_audit_org_id',
       'nc_202604160000_docs_in_data',
       'nc_202604200002_trash_cleanup_due_at',
+      'nc_202604220000_button_open_form',
       'nc_202603180000_user_mfa',
     ]);
   }
@@ -226,14 +226,14 @@ export default class XcMigrationSourcev0 {
         return nc_202604071200_default_org;
       case 'nc_202604071201_scim_config_default_role':
         return nc_202604071201_scim_config_default_role;
-      case 'nc_202604090000_button_open_form':
-        return nc_202604090000_button_open_form;
       case 'nc_202604100000_audit_org_id':
         return nc_202604100000_audit_org_id;
       case 'nc_202604160000_docs_in_data':
         return nc_202604160000_docs_in_data;
       case 'nc_202604200002_trash_cleanup_due_at':
         return nc_202604200002_trash_cleanup_due_at;
+      case 'nc_202604220000_button_open_form':
+        return nc_202604220000_button_open_form;
       case 'nc_202603180000_user_mfa':
         return nc_202603180000_user_mfa;
     }
