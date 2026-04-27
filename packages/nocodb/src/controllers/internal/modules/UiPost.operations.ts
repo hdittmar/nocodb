aa8d77775cf16239db9c908c28d5c26d4305ef45
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2026-04-27 07:44:09 +0000

    chore: move code in ee

--- packages/nocodb/src/controllers/internal/modules/UiPost.operations.ts
+++ packages/nocodb/src/controllers/internal/modules/UiPost.operations.ts
@@ -99,7 +99,6 @@ export class UiPostOperations
     'hookTrigger' as const,
     'hookFilterCreate' as const,
     'buttonFilterCreate' as const,
-    'formEditTokenGenerate' as const,
     'gridViewCreate' as const,
     'formViewCreate' as const,
     'galleryViewCreate' as const,
@@ -446,11 +445,6 @@ export class UiPostOperations
           viewId: req.query.viewId,
           grid: payload,
           req,
-        });
-      case 'formEditTokenGenerate':
-        return await this.formsService.generateEditToken(context, {
-          columnId: req.query.columnId as string,
-          rowId: req.query.rowId as string,
         });
       case 'formViewUpdate':
         return await this.formsService.formViewUpdate(context, {
