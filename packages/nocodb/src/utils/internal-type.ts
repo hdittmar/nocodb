867e160e38bc1987ab1dc29c909b8b19d6f59669
DarkPhoenix2704 <anbarasun123@gmail.com>
2026-04-27 12:00:16 +0000

    fix: review corrections

--- packages/nocodb/src/utils/internal-type.ts
+++ packages/nocodb/src/utils/internal-type.ts
@@ -99,7 +99,8 @@ export type InternalPOSTResponseType = Promise<
       columns: any[];
       previewData: any[];
       totalSampleRows: number;
-      detectedDelimiter: string;
+      totalRows: number;
+      detectedDelimiter?: string;
     }
   | { id: string; name?: string }
 >;
